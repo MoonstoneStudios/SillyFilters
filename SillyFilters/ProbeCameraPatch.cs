f37eac177bd8b7d59e5c81bcad6d7d80d6f2036d
Moonstone Studios <61355352+MoonstoneStudios@users.noreply.github.com>
2023-10-10 07:18:06 -0700

    Separate image combination logic.
    Also tried to make it easier to read.

--- SillyFilters/ProbeCameraPatch.cs
+++ SillyFilters/ProbeCameraPatch.cs
@@ -12,25 +12,39 @@ namespace SillyFilters
     [HarmonyPatch]
     public class ProbeCameraPatch
     {
-        /// <summary>
-        /// Override what is returned by the launcher.
-        /// </summary>
+        private static SillyFilters ModInstance => SillyFilters.instance;
+
+        /// <summary>Override what is returned by the launcher.</summary>
         /// <param name="__result">The image that the probe produces.</param>
         [HarmonyPostfix]
         [HarmonyPatch(typeof(ProbeCamera), nameof(ProbeCamera.TakeSnapshot))]
         public static void ProbeCamera_TakeSnapshot_Postfix(ref RenderTexture __result)
         {
             // don't change anything
-            if (SillyFilters.instance.imageFileName == "none.png") return;
+            if (ModInstance.imageFileName == "none.png") return;
 
             // image loading credit
             // https://github.com/Pau318/OuterPictures
-            var overlay = SillyFilters.instance.ModHelper.Assets.GetTexture("Assets\\" + SillyFilters.instance.imageFileName);
+            var overlay = ModInstance.ModHelper.Assets.GetTexture("Assets\\" + SillyFilters.instance.imageFileName);
             var result2d = __result.ToTexture2D();
 
-            // use alpha blending to merge images
             var basePixels = result2d.GetPixels();
             var overlayPixels = overlay.GetPixels();
+
+            // add filter to the probe image.
+            var newImage = CombineImages(basePixels, overlayPixels);
+
+            // turn the texture2d into a render texture and return.
+            Graphics.CopyTexture(newImage, __result);
+        }
+
+
+        /// <summary>Combine two images using alpha compositing.</summary>
+        /// <param name="basePixels">The base image's pixels.</param>
+        /// <param name="overlayPixels">The overlay image's pixels.</param>
+        /// <param name="newImage">A reference to the resulting image.</param>
+        private static Texture2D CombineImages(Color[] basePixels, Color[] overlayPixels)
+        {
             var newImage = new Texture2D(512, 512, TextureFormat.RGBA32, 1, false);
 
             // 512 image size.
@@ -38,20 +52,24 @@ namespace SillyFilters
             {
                 for (int y = 0; y < 512; y++)
                 {
-                    // https://softwareengineering.stackexchange.com/a/212813
+                    // instead of using Texture2D.GetPixel(x, y) we use Texture2D.GetPixels() (see above)
+                    // to convert all pixels into Color classes in one go
+                    // instead of creating new objects each loop.
+
                     // convert the 2d coords to the index
+                    // https://softwareengineering.stackexchange.com/a/212813
                     var i = x + 512 * y;
 
-                    // i accidentally swapped these two in the formula
-                    // so here is my fix lol.
-                    var basePixel = overlayPixels[i];
-                    var overlayPixel = basePixels[i];
+                    var basePixel = basePixels[i];
+                    var overlayPixel = overlayPixels[i];
 
                     // https://en.wikipedia.org/wiki/Alpha_compositing
-                    var newAlpha = basePixel.a + (overlayPixel.a * (1f - basePixel.a));
-                    var newR = ((basePixel.r * basePixel.a) + (overlayPixel.r * overlayPixel.a * (1f - basePixel.a))) / newAlpha;
-                    var newG = ((basePixel.g * basePixel.a) + (overlayPixel.g * overlayPixel.a * (1f - basePixel.a))) / newAlpha;
-                    var newB = ((basePixel.b * basePixel.a) + (overlayPixel.b * overlayPixel.a * (1f - basePixel.a))) / newAlpha;
+                    var invAlpha = 1f - overlayPixel.a;
+                    var newAlpha = overlayPixel.a + (basePixel.a * invAlpha);
+
+                    var newR = ((overlayPixel.r * overlayPixel.a) + (basePixel.r * basePixel.a * invAlpha)) / newAlpha;
+                    var newG = ((overlayPixel.g * overlayPixel.a) + (basePixel.g * basePixel.a * invAlpha)) / newAlpha;
+                    var newB = ((overlayPixel.b * overlayPixel.a) + (basePixel.b * basePixel.a * invAlpha)) / newAlpha;
 
                     newImage.SetPixel(x, y, new Color(newR, newG, newB, newAlpha));
                 }
@@ -59,9 +77,7 @@ namespace SillyFilters
 
             // apply our changes and send them to the GPU
             newImage.Apply();
-
-            // trun the texture2d into a render texture and return.
-            Graphics.CopyTexture(newImage, __result);
+            return newImage;
         }
     }
 }
